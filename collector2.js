3c117280c766daf6a0598d31456307c90566a42a
Phil Davis <phildavis17@gmail.com>
2018-01-21 22:02:33 -0500

    further work on Collector

--- collector2.js
+++ collector2.js
@@ -2,12 +2,13 @@ function init(){
     //get the variables from the page
     //erturns and object with all the variables
     var userVars = {};
-    userVars.nSize = document.getElementById("nSize");
-    userVars.boxSize = document.getElementById("boxSize");
-    userVars.dupesInBox = document.getElementById("dupesInBox");
-    userVars.runs = document.getElementById("runs");
+    userVars.nSize = document.getElementById("nSize").value;
+    userVars.boxSize = document.getElementById("boxSize").value;
+    userVars.dupesInBox = document.getElementById("dupesInBox").checked;
+    userVars.runs = document.getElementById("runs").value;
     return userVars;
 }
+
 
 function buy(col, size, dup){
     //returns an array of size boxSize containing valid numbers
@@ -15,12 +16,17 @@ function buy(col, size, dup){
     var box = [];
 
     function select(col){
-        return Math.floor(Math.random() * col.length);
+        return Math.floor(Math.random() * col);
     }
 
     for (i=0; i<size; i++){
-        box.push(select(col))
+        var thisBox = [];
+        var newNum = select(col);
+        if (!box.includes(newNum)){
+            box.push(newNum);
+        }
     }
+
 
     return box;
 }
@@ -39,7 +45,11 @@ function buyUntilFull(){
 }
 
 function main(){
+    console.log("This is a test");
     //executes the script
-    settings = init();
-    buyUntilFull(settings);
+    var settings = init();
+    //console.log(settings.nSize);
+    //console.log(settings.dupesInBox);
+    var test = buy(settings.nSize, settings.boxSize, settings.dupesInBox);
+    console.log(test);
 }
