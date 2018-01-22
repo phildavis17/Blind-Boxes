349dffb962d80e951c693cca828d6865d443a5b2
Phil Davis <phildavis17@gmail.com>
2018-01-21 22:53:08 -0500

    Buy works, buyUntilFull works

--- collector2.js
+++ collector2.js
@@ -6,42 +6,65 @@ function init(){
     userVars.boxSize = document.getElementById("boxSize").value;
     userVars.dupesInBox = document.getElementById("dupesInBox").checked;
     userVars.runs = document.getElementById("runs").value;
+    //Find and alert for bugs
     return userVars;
 }
 
 
 function buy(col, size, dup){
     //returns an array of size boxSize containing valid numbers
-
     var box = [];
 
     function select(col){
+        //retunrs an in bounds number
         return Math.floor(Math.random() * col);
     }
 
-    for (i=0; i<size; i++){
-        var thisBox = [];
-        var newNum = select(col);
-        if (!box.includes(newNum)){
-            box.push(newNum);
+    //build a dupe free box, unless there can be dupes
+    if (!dup){
+        do {
+            var thisBox = [];
+            var newNum = select(col);
+            if (!box.includes(newNum)){
+                box.push(newNum);
+            }
+        } while (box.length < size);
+    } else {
+        for (var i=0; i<size; i++){
+            box.push(select(col))
         }
     }
-
-
     return box;
 }
 
-function buyUntilFull(){
+function buyUntilFull(vars){
     //buys boxes until the collection is complete
     //returns the number of tries it took
 
-    //build the reference Collection
+    //build an empty array nSize long
+    var current = [];
+    do {
+        current.push(0);
+    }while (current.length < vars.nSize);
+    console.log("collection built.");
 
-    //while not done
-    //  buy
-    //  check
+    var done = false;
+    var tries = 0;
 
-    //return number of tries
+    do{
+        tries++;
+        var newBox = buy(vars.nSize, vars.boxSize, vars.dupesInBox);
+        console.log("this box: " + newBox);
+        for(var n in newBox){
+            current[newBox[n]]++;
+        }
+        if(!current.includes(0)){
+            done = true;
+        }
+        console.log(current);
+    } while (!done);
+
+    return tries;
 }
 
 function main(){
@@ -50,6 +73,6 @@ function main(){
     var settings = init();
     //console.log(settings.nSize);
     //console.log(settings.dupesInBox);
-    var test = buy(settings.nSize, settings.boxSize, settings.dupesInBox);
+    var test = buyUntilFull(settings);
     console.log(test);
 }
